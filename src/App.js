ab08d337112a22a4ea2d43e204da6a078863e05a
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-11-18 10:05:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
 import Cadastro from './screens/Cadastro';
 import Servicos from './screens/Servicos';
+import Sucesso from './screens/Sucesso';
+import Erro from './screens/Erro';
 import './App.css';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/servicos" element={<Servicos />} />
+          <Route path="/sucesso" element={<Sucesso />} />
+          <Route path="/erro" element={<Erro />} />
         </Routes>
       </div>
     </Router>
