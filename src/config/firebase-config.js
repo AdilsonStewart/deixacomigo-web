d73aa92d77a7835f59b5de1594259ac09e9e8dec
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-11-19 16:17:02 -0300

    Create firebase-config.js

---
+++ src/config/firebase-config.js
@@ -0,0 +1,9 @@
+// Configuração hardcoded temporária para teste
+export const firebaseConfig = {
+  apiKey: "AIzaSyC1Xv2mPNf4s2oY-Jeh2ev3x0O6qkKNqt4",
+  authDomain: "deixacomigo-727ff.firebaseapp.com",
+  projectId: "deixacomigo-727ff",
+  storageBucket: "deixacomigo-727ff.firebasestorage.app",
+  messagingSenderId: "304342645043",
+  appId: "1:304342645043:web:893af23b41547a29a1a646"
+};
