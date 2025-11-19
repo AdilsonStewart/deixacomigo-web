f5aa9058801a2d2a646a5fc5572e11cd7910c0ac
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-11-19 15:32:32 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,20 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// Suas credenciais do Firebase
 const firebaseConfig = {
-  // SUAS CREDENCIAIS DO FIREBASE AQUI
-  apiKey: "sua-api-key",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto-id",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "seu-sender-id",
-  appId: "seu-app-id"
+  apiKey: "AIzaSyC1Xv2mPNf4s2oY-Jeh2ev3x0O6qkKNqt4",
+  authDomain: "deixacomigo-727ff.firebaseapp.com",
+  projectId: "deixacomigo-727ff",
+  storageBucket: "deixacomigo-727ff.firebasestorage.app",
+  messagingSenderId: "304342645043",
+  appId: "1:304342645043:web:893af23b41547a29a1a646"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Storage
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export default app;
