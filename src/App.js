bc1f2eb371ba5f349cb014013d55b7f65397c7b4
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-11-20 10:53:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// IMPORTS DAS TELAS
 import Home from './screens/Home';
 import Cadastro from './screens/Cadastro';
 import Servicos from './screens/Servicos';
 import Sucesso from './screens/Sucesso';
 import Erro from './screens/Erro';
-import AudioRecordPage from './screens/AudioRecordPage'; // ← NOME CORRETO AGORA!
+import AudioRecordPage from './screens/AudioRecordPage';
 import Agendamento from './screens/Agendamento';
 import Saida from './screens/Saida';
 import VideoRecordPage from './screens/VideoRecordPage';
 import AdminDashboard from './screens/AdminDashboard';
 import Sucesso2 from './screens/Sucesso2';
+
 import './App.css';
 
 function App() {
@@ -25,10 +28,18 @@ function App() {
           <Route path="/erro" element={<Erro />} />
           <Route path="/agendamento" element={<Agendamento />} />
           <Route path="/saida" element={<Saida />} />
+
+          {/* GRAVADOR DE VÍDEO */}
           <Route path="/videorecorder" element={<VideoRecordPage />} />
+
+          {/* GRAVADOR DE ÁUDIO */}
+          <Route path="/audiorecorder" element={<AudioRecordPage />} />
+
+          {/* ADM */}
           <Route path="/admin/painel" element={<AdminDashboard />} />
+
+          {/* SUCESSO DO PAGAMENTO QUE VAI PARA VÍDEO */}
           <Route path="/sucesso2" element={<Sucesso2 />} />
-          <Route path="/audiorecorder" element={<AudioRecordPage />} /> {/* ← Corrigido! */}
         </Routes>
       </div>
     </Router>
@@ -36,5 +47,3 @@ function App() {
 }
 
 export default App;
-
-
