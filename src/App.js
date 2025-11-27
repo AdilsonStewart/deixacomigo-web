862c5f52160671c293a4e1ec048b0ac3329bd8a9
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-11-27 13:44:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ import Agendamento from './screens/Agendamento';
 import Saida from './screens/Saida';
 import VideoRecordPage from './screens/VideoRecordPage';
 import AdminDashboard from './screens/AdminDashboard';
-
-// APENAS AS PÁGINAS QUE VOCÊ QUER MANTER
-import Pago2 from './screens/Pago2';   // ← esse arquivo você já tem e funciona
+import Pago2 from './screens/Pago2';   // Para áudio
+import Sucesso2 from './screens/Sucesso2'; // Para vídeo - você precisa criar este componente
 
 import './App.css';
 
@@ -27,13 +26,13 @@ function App() {
           <Route path="/erro" element={<Erro />} />
           <Route path="/agendamento" element={<Agendamento />} />
           <Route path="/saida" element={<Saida />} />
-          <Route path="/videorecorder" element={<VideoRecordPage />} />
+          <Route path="/video-record" element={<VideoRecordPage />} /> {/* Alterado de '/videorecorder' para '/video-record' */}
           <Route path="/audiorecorder" element={<AudioRecordPage />} />
           <Route path="/admin/painel" element={<AdminDashboard />} />
 
-          {/* ROTAS NOVAS E DEFINITIVAS DA CORA */}
-          <Route path="/pago" element={<Pago2 />} />
-          <Route path="/pago/" element={<Pago2 />} />
+          {/* ROTAS DE RETORNO DA CORA */}
+          <Route path="/pago" element={<Pago2 />} />   // Para áudio
+          <Route path="/sucesso2" element={<Sucesso2 />} /> // Para vídeo
         </Routes>
       </div>
     </Router>
