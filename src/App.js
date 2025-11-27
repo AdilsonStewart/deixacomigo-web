33fbd0cd2b2706def6090f6d3565a69629fd47c7
AdilsonStewart <144806733+AdilsonStewart@users.noreply.github.com>
2025-11-27 14:02:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Agendamento from './screens/Agendamento';
 import Saida from './screens/Saida';
 import VideoRecordPage from './screens/VideoRecordPage';
 import AdminDashboard from './screens/AdminDashboard';
-import Pago2 from './screens/Pago2';   // Para áudio
-import Sucesso2 from './screens/Sucesso2'; // Para vídeo - você precisa criar este componente
+import pago from './screens/pago';   // Para áudio - minúsculo
+import pago2 from './screens/pago2'; // Para vídeo - minúsculo
 
 import './App.css';
 
@@ -26,13 +26,13 @@ function App() {
           <Route path="/erro" element={<Erro />} />
           <Route path="/agendamento" element={<Agendamento />} />
           <Route path="/saida" element={<Saida />} />
-          <Route path="/video-record" element={<VideoRecordPage />} /> {/* Alterado de '/videorecorder' para '/video-record' */}
+          <Route path="/video-record" element={<VideoRecordPage />} />
           <Route path="/audiorecorder" element={<AudioRecordPage />} />
           <Route path="/admin/painel" element={<AdminDashboard />} />
 
-          {/* ROTAS DE RETORNO DA CORA */}
-          <Route path="/pago" element={<Pago2 />} />   // Para áudio
-          <Route path="/sucesso2" element={<Sucesso2 />} /> // Para vídeo
+          {/* ROTAS DE RETORNO DA CORA - APENAS PAGO E PAGO2 */}
+          <Route path="/pago" element={<pago />} />     {/* Para áudio */}
+          <Route path="/pago2" element={<pago2 />} />   {/* Para vídeo */}
         </Routes>
       </div>
     </Router>
